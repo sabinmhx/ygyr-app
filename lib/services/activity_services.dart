c03223cfd84b2bd5ac814f58561e49793a085bc9
sabinmhx <sabinmhx@gmail.com>
2024-05-11 18:49:17 +0545

    refactor: return model instead of list of data

--- lib/services/activity_services.dart
+++ lib/services/activity_services.dart
@@ -5,17 +5,13 @@ import 'package:flutter/services.dart';
 import 'package:ygyr/model/activity_model.dart';
 
 class ActivityServices {
-  Future<List<Data>> getActivity() async {
+  Future<ActivityModel?> getActivity() async {
     try {
-      final response = await rootBundle.loadString("assets/activity.json");
-      final Map<String, dynamic> data = jsonDecode(response);
-      final List<dynamic> dataList = data['data'];
-      final List<Data> activities =
-          dataList.map((json) => Data.fromJson(json)).toList();
-      return activities;
+      final jsonString = await rootBundle.loadString('assets/activity.json');
+      return ActivityModel.fromJson(json.decode(jsonString));
     } catch (e) {
       log('Error fetching activity data: $e');
-      return [];
+      return null;
     }
   }
 }
